c1cc3f5b5ef3086d3f998fa0b1270b7ec5b7d584
Gargi Gupta <gargig02@gmail.com>
2021-11-13 17:52:57 +0530

    commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Recipe_site',
         'USER': 'root',
-        'PASSWORD': 'Shruti@236',
+        'PASSWORD': 'MySQL_ITW2',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
