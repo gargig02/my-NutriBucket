eb8993847d13ea640638714511e4ca5175c2be52
Shruti-codes236 <76914026+Shruti-codes236@users.noreply.github.com>
2021-11-24 12:29:22 +0530

    chatbox

--- mysite/mysite/asgi.py
+++ mysite/mysite/asgi.py
@@ -9,8 +9,27 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+import django
+from channels.auth import AuthMiddlewareStack
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_asgi_application()
+
+# application = ProtocolTypeRouter({
+#     "http": AsgiHandler(),
+#     # Just HTTP for now. (We can add other protocols later.)
+# })
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+    # Just HTTP for now. (We can add other protocols later.)
+})
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as users_views
@@ -39,7 +40,8 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
          name="password_reset_complete"),
     path('profile/', users_views.profile, name="profile"),
-
+    path('chat/', include('chat.urls')),
+    
 ]
 
 
